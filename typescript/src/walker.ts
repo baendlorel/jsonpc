71d7f082a0275d55f3ab6dba02d77e26c9b99632
aldia_mechdream <futami16237@gmail.com>
2026-08-30 13:15:29 +0800

    feat: 取消了afterChar； left right改为const enum

--- typescript/src/walker.ts
+++ typescript/src/walker.ts
@@ -2,12 +2,17 @@ import { _noop } from './common.js';
 
 export const isSpace = (t: unknown) => t === ' ' || t === '\t' || t === '\r' || t === '\n';
 
+export const enum Side {
+  Left,
+  Right,
+}
+
 interface WalkerHandlerArgs {
   index: number;
 }
 
 interface WalkerHandlerArgsWithSides extends WalkerHandlerArgs {
-  side: 'left' | 'right';
+  side: Side;
 }
 
 /**
@@ -24,27 +29,21 @@ interface WalkerOptions {
   onBracket: (args: WalkerHandlerArgsWithSides) => void;
 
   /**
-   * Triggers when char is not something above.
+   * Triggers when char is none of above.
    */
   onOther: (args: WalkerHandlerArgs) => void;
-
-  /**
-   * Triggers when char is not something above, and is not whitespace.
-   */
-  afterChar: (args: WalkerHandlerArgs) => void;
 }
 
 export function walk(text: string, handlers: Partial<WalkerOptions>) {
   const {
     start = 0,
-    end = text.length - 1,
+    end = text.length,
     onComma = _noop,
     onColon = _noop,
     onQuote = _noop,
     onBrace = _noop,
     onBracket = _noop,
     onOther = _noop,
-    afterChar = _noop,
   } = handlers;
 
   let inString = false;
@@ -56,7 +55,6 @@ export function walk(text: string, handlers: Partial<WalkerOptions>) {
     // Handle escape sequences
     if (escaped) {
       escaped = false;
-      afterChar({ index: i });
       continue;
     }
 
@@ -69,8 +67,7 @@ export function walk(text: string, handlers: Partial<WalkerOptions>) {
     if (inString) {
       if (char === '"') {
         inString = false;
-        onQuote({ index: i, side: 'right' });
-        afterChar({ index: i });
+        onQuote({ index: i, side: Side.Right });
       }
       continue;
     }
@@ -85,24 +82,23 @@ export function walk(text: string, handlers: Partial<WalkerOptions>) {
         break;
       case '"':
         inString = true;
-        onQuote({ index: i, side: 'left' });
+        onQuote({ index: i, side: Side.Left });
         break;
       case '{':
-        onBrace({ index: i, side: 'left' });
+        onBrace({ index: i, side: Side.Left });
         break;
       case '}':
-        onBrace({ index: i, side: 'right' });
+        onBrace({ index: i, side: Side.Right });
         break;
       case '[':
-        onBracket({ index: i, side: 'left' });
+        onBracket({ index: i, side: Side.Left });
         break;
       case ']':
-        onBracket({ index: i, side: 'right' });
+        onBracket({ index: i, side: Side.Right });
         break;
       default:
         onOther({ index: i });
         break;
     }
-    afterChar({ index: i });
   }
 }
